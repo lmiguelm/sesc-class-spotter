604959dc709b5167fc4da0ada47ef873c5d092cc
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2024-03-23 10:32:46 -0300

    test: bot

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,60 @@
 import cron from 'node-cron'
-import { Channel, Client, GatewayIntentBits } from 'discord.js'
+import { Client, GatewayIntentBits } from 'discord.js'
 
-import { Turma } from "./models/turma"
-import { COURSES } from "./utils/watched-courses"
+import { Turma } from './models/turma'
+import { COURSES } from './utils/watched-courses'
 
 export const bot = new Client({
-  intents: [
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.Guilds
-  ]
+  intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds],
 })
 
 bot.login(process.env.DISCORD_TOKEN)
 
 bot.on('ready', (discord) => {
-	console.log(`Bot started with: ${bot.user?.tag}`);
+  console.log(`Bot started with: ${bot.user?.tag}`)
 
   const channel = discord.channels.cache.get('1220886122914123830')
 
-  cron.schedule('* * * * *', () => main(channel!));
-});
+  cron.schedule('* * * * *', () => main(channel!))
+})
 
 async function fetchAvailability() {
   console.log('Fetching availabilities...')
-  
-  const response = await fetch('https://centralrelacionamento-api.sescsp.org.br/inscricoes-online/cursos?unidades=86&categorias=10009')
+
+  const response = await fetch(
+    'https://centralrelacionamento-api.sescsp.org.br/inscricoes-online/cursos?unidades=86&categorias=10009',
+  )
 
   if (!response.ok) {
-    console.log('Unable to fetch courses.')
+    throw new Error('Unable to fetch courses.')
   }
 
-  const courses = await response.json() as Turma[]
+  const courses = (await response.json()) as Turma[]
 
-  const filteredCourses = courses.filter(course => Object.values(COURSES).includes(course.turmaId))
-  const availableCourses = filteredCourses.filter(course => course.vagasDisponiveis > 0)
+  const filteredCourses = courses.filter((course) =>
+    Object.values(COURSES).includes(course.turmaId),
+  )
+
+  const availableCourses = filteredCourses.filter(
+    (course) => course.vagasDisponiveis > 0,
+  )
 
   return availableCourses
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 async function main(bot: any) {
-  const availableCourses = await fetchAvailability()
+  try {
+    bot.send('test')
+    const availableCourses = await fetchAvailability()
+    bot.send('test: ', availableCourses)
 
-  availableCourses.forEach(course => {
-    bot.send(`@here\n\nHá ${course.vagasDisponiveis} ${course.vagasDisponiveis > 1 ? 'vagas disponíveis' : 'vaga disponível'}  para o curso "${course.cursoTitulo}". Corre!!!!!`)
-  })
+    availableCourses.forEach((course) => {
+      bot.send(
+        `@here\n\nHá ${course.vagasDisponiveis} ${course.vagasDisponiveis > 1 ? 'vagas disponíveis' : 'vaga disponível'}  para o curso "${course.cursoTitulo}". Corre!!!!!`,
+      )
+    })
+  } catch {
+    bot.send('Unable to fetch courses.')
+  }
 }
--- src/utils/watched-courses.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.COURSES = void 0;
-exports.COURSES = {
-    CLUBE_DO_FUTSAL: '44336-86',
-    CLUBE_DO_VOLEI: '44171-86'
-};
